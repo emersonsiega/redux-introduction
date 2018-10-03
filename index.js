1c6f7fd7ba02430d09faf2c775bc02d97a5a47d2
Emerson Siega <emersonsiega@hotmail.com>
2018-10-02 21:34:15 -0300

    feat: Control subscriptions

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ function createStore() {
      */
 
      let state
+     let listeners = []
 
      const getState = () => state
 
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        // unsubscribe function
+        return () => {
+            listeners = listeners.filter( l => l !== listener )
+        }
+    }
+
      return {
-         getState
+         getState,
+         subscribe
      }
 }
