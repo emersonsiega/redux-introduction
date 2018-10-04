968751791dd2de02517d25f222e8b9fda19d09a7
Emerson Siega <emersonsiega@hotmail.com>
2018-10-04 07:41:27 -0300

    feat: Add remove todo and toggle todo features

--- todoApp.js
+++ todoApp.js
@@ -1,16 +1,24 @@
 const redux = require('./index')
 
 const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
 
 /**
  * Reducer function, to update the state
  */
 function todos(state = [], action) {
-    if (action.type === ADD_TODO) {
-        return state.concat([action.todo])
+    switch ( action.type ) {
+        case ADD_TODO: 
+            return state.concat([action.todo])
+        case REMOVE_TODO:
+            return state.filter( todo => todo.id != action.id )
+        case TOGGLE_TODO:
+            return state.map( todo => todo.id !== action.id ? todo : 
+                Object.assign({}, todo, { complete: !todo.complete }) )
+        default:
+            return state    
     }
-    
-    return state
 }
 
 const store = redux.createStore(todos)
